d585d99af17d6ea0a9e476ad9d45ea38532f8f2b
Amash-rehmani123 <amash.rehmani@gmail.com>
2024-05-13 12:12:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+//Display a colourful welcome message
+console.log(chalk.bold.rgb(204, 204, 204)(`\n \t\t <<< ================================================= >>>\n`));
+console.log(chalk.bold.rgb(204, 204, 204)(chalk.magenta.bold(`\t\t\t Welcome to \` Amashta Rehmani \` Word Counter\n`)));
+console.log(chalk.bold.rgb(204, 204, 204)(`\t\t <<< ================================================= >>>`));
+let userInput = await inquirer.prompt({
+    message: "Enter your message to count the words!",
+    type: "input",
+    name: "Paragraph"
+});
+let para = userInput.Paragraph;
+let cleanPara = para.trim();
+let wordsArray = cleanPara.split(' ');
+let filteredWords = wordsArray.filter(word => word !== '');
+console.log(chalk.yellowBright.bold(`\n \t\t You typed ${filteredWords.length} words.`));
+console.log(chalk.yellowBright.bold(`\n \t\t You typed ${cleanPara}.length} characters.`));
---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Display a colourful welcome message
+console.log(chalk.bold.rgb(204,204,204)(`\n \t\t <<< ================================================= >>>\n`));
+console.log(chalk.bold.rgb(204,204,204)(chalk.magenta.bold(`\t\t\t Welcome to \` Amashta Rehmani \` Word Counter\n`)));
+console.log(chalk.bold.rgb(204,204,204)(`\t\t <<< ================================================= >>>`));
+
+let userInput = await inquirer.prompt({
+    message: "Enter your message to count the words!",
+    type: "input",
+    name: "Paragraph"
+});
+let para : string = userInput.Paragraph;
+let cleanPara : string = para.trim();
+let wordsArray : string[] = cleanPara.split(' ');
+let filteredWords = wordsArray.filter(word => word !== '');
+console.log(chalk.yellowBright.bold(`\n \t\t You typed ${filteredWords.length} words.`));
+console.log(chalk.yellowBright.bold(`\n \t\t You typed ${cleanPara}.length} characters.`));
